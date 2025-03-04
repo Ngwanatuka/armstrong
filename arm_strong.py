cbc64dd18f0d33d765a7de7590bbc91b5f2cfbc9
ngmolepjhb024 <ngmolepjhb024@student.wethinkcode.co.za>
2025-03-04 13:01:32 +0200

    Add test cases for Armstrong number function

--- arm_strong.py
+++ arm_strong.py
@@ -2,3 +2,14 @@ def is_arm_strong(number):
     num = list(map(int, str(number)))
     n = len(num)
     return number == sum([x**n for x in num])
+
+n = 153
+print(is_arm_strong(n)) # True
+n = 370
+print(is_arm_strong(n)) # True
+n = 9474
+print(is_arm_strong(n)) # True
+n = 123
+print(is_arm_strong(n)) # False
+n = 142
+print(is_arm_strong(n)) # False
